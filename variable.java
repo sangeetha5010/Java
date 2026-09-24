9640b9f040c3d7b40e88ff3a5d7b404b88d0fbe1
Sangeetha <sangeethauk08@gmail.com>
2026-09-24 21:19:40 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,12 @@
+class variable{
+    public static void main() {
+        System.out.print("Hello World!\n");
+        System.out.println(3+8);
+        System.out.println(7+9);
+        int num=5;
+        System.out.println(num);
+        System.out.println("Welcome to First Class!");
+
+    
+    }
+}
